b57bfcad0348adfee03ab53d426c7d5c89551895
subendra mandal <subendra483@gmail.com>
2023-06-30 03:24:10 +0545

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 require("dotenv").config();
 require("./connect/db");
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ const userRoute = require("./routes/UserRoutes");
 
 app.use(express.json());
 app.use("/api", userRoute);
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Welcome to homepage");
